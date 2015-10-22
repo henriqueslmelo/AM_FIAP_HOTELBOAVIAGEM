05dbc10c94e2c2bfa13376a4904888f3c7fe2f9f
Monica Silva <monicassilva15@gmail.com>
2015-10-22 12:27:13 -0200

    DADOS DE CONEXAO REMOTA INSERIDOS

--- src/br/com/fiap/am/conexao/Conexao.java
+++ src/br/com/fiap/am/conexao/Conexao.java
@@ -19,7 +19,11 @@ public class Conexao {
 		DriverManager.setLoginTimeout(100);
 
 		try {
-			return DriverManager.getConnection("jdbc:oracle:thin:/:@192.168.60.15:1521:ORCL", "OPS$RM75202", "041288");
+			//return DriverManager.getConnection("jdbc:oracle:thin:/:@192.168.60.15:1521:ORCL", "OPS$RM75202", "041288");
+			//USADO NA FIAP
+			return DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "OPS$RM71962", "140486");   
+			// USADO REMOTO
+
 		} catch (Exception e) {
 			throw new Excecao("Ocorreu um erro", e);
 		}
