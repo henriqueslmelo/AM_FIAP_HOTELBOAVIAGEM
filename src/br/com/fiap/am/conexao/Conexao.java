4ef6811d26641bea19eee7fa2ce7c3a8e5da0a9e
Monica Silva <monicassilva15@gmail.com>
2015-10-26 14:30:35 -0200

    Implementação do javadoc na classe conexão

--- src/br/com/fiap/am/conexao/Conexao.java
+++ src/br/com/fiap/am/conexao/Conexao.java
@@ -1,10 +1,18 @@
 package br.com.fiap.am.conexao;
 
+/**
+ * Classe para conexão com o banco de dados Oracle
+ * 
+ * 
+ * @author Bruno Abdante
+ * @author Henrique Melo
+ * @author Monica Silva
+ *
+ */
+
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import br.com.fiap.am.exception.Excecao;
 
@@ -15,18 +23,24 @@ public class Conexao {
 			Class.forName("oracle.jdbc.OracleDriver");
 		} catch (Exception e) {
 			throw new Excecao("Ocorreu um erro", e);
+		
 		}
 		DriverManager.setLoginTimeout(100);
 
 		try {
 			//return DriverManager.getConnection("jdbc:oracle:thin:/:@192.168.60.15:1521:ORCL", "OPS$RM75202", "041288");
 			//USADO NA FIAP
-			return DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "OPS$RM71962", "140486");   
-			// USADO REMOTO
-
+			return DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "OPS$RM71962", "140486");
+			
+		    /** Método para retorno do driver de conexao
+		     * 
+		     */
+        		 
 		} catch (Exception e) {
 			throw new Excecao("Ocorreu um erro", e);
 		}
 
 	}
 }
+
+
